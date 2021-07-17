555571b7a6e520795f33f8a8510a9b7a80df68b6
BlizzardOfOzzy <ojd4hh@virginia.edu>
2021-07-16 21:18:47 -0400

    Added trivial database put and get

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+from flask import json
+from flask.json import jsonify
+from pymongo import MongoClient
+from bson import json_util
 
 app = Flask(__name__)
 
-@app.route("/")
-def sample():
-    return "Test"
+client = MongoClient('mongodb://localhost')
+database = client.database
+
+@app.route("/", methods=['PUT'])
+def put_record():
+    database.example.insert_one(request.json)
+    return "Success"
+
+@app.route("/", methods=['GET'])
+def get_record():
+    return json_util.dumps({'records': list(database.example.find({}))})
